7d2859bad56674f198b5b67016ddaea9120aa85f
Akira Ouchi <akkiesoft@marokun.net>
2013-12-19 00:06:27 +0900

    書類の種類を選べるようにした

--- mikutter_shakuyoushou.rb
+++ mikutter_shakuyoushou.rb
@@ -8,6 +8,10 @@ Plugin.create :mikutter_shakuyoushou do
     role: :timeline) do |opt|
 
     opt.messages.each { |m|
+
+      # 書類の種類
+      list = ["借　用　証","領　収　書","請　求　書"]
+    
       # Create the dialog
       dialog = Gtk::Dialog.new("借用証を発行する",
                                $main_application_window,
@@ -18,17 +22,28 @@ Plugin.create :mikutter_shakuyoushou do
       dialog.vbox.add(Gtk::Label.new("借用している金額を入力してください"))
       kingaku_box = Gtk::Entry.new
       dialog.vbox.add(kingaku_box)
+
+      dialog.vbox.add(Gtk::Label.new("書類の種類を選択してください"))
+      mogmog = Gtk::ComboBox.new(true)
+      dialog.vbox.add(mogmog)
+      list.each do |s|
+      mogmog.append_text(s)
+    end
+    mogmog.set_active(0)
+
       dialog.show_all
 
       result = dialog.run
       kingaku = nil
+      mogmogindex = nil
       if result == Gtk::Dialog::RESPONSE_OK
         kingaku = kingaku_box.text
+        mogmogindex = mogmog.active
       end
       dialog.destroy
 
       if kingaku != nil
-        Service.primary.post(:message =>"　　　　　借　用　証
+        Service.primary.post(:message =>"　　　　　#{list[mogmogindex]}
  
 @#{m.user.to_s} 様
 　　　　　　　　#{Time.now.strftime('%Y年%m月%d日')}
